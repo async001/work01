9e8c8961c0a56738cd6aa033e963007afd71d799
async001 <161574089+async001@users.noreply.github.com>
2024-03-12 21:58:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.write("Hello world!");
+function sayHello(){
+  var response = prompt("What is your sweet name?");
+  alert("Hello " + response + ", have a nice day!");
+};
+
+function showTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSize = "25px";
+  x.style.color = "purple";
+};
